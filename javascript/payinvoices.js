b627c57a62180dcfbfef0b1cb3e496d610ef92ee
AshleyH <ladynirvana@hotmail.com>
2018-11-12 15:37:44 -0700

    adding excel doc

---
+++ javascript/payinvoices.js
@@ -0,0 +1,11 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAzLuXMlFXEt3-fUHbaF5BxNpb2_aJNw-Q",
+    authDomain: "payinvoices-9d71a.firebaseapp.com",
+    databaseURL: "https://payinvoices-9d71a.firebaseio.com",
+    projectId: "payinvoices-9d71a",
+    storageBucket: "payinvoices-9d71a.appspot.com",
+    messagingSenderId: "767219632605"
+  };
+  firebase.initializeApp(config);
+  
