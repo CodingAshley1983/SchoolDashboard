4a136a4275beb0636b07009ef769bad2e57e4299
AshleyH <ladynirvana@hotmail.com>
2018-11-12 21:45:47 -0700

    payment screen

--- javascript/payinvoices.js
+++ javascript/payinvoices.js
@@ -1,11 +1,19 @@
-  // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyAzLuXMlFXEt3-fUHbaF5BxNpb2_aJNw-Q",
-    authDomain: "payinvoices-9d71a.firebaseapp.com",
-    databaseURL: "https://payinvoices-9d71a.firebaseio.com",
-    projectId: "payinvoices-9d71a",
-    storageBucket: "payinvoices-9d71a.appspot.com",
-    messagingSenderId: "767219632605"
-  };
-  firebase.initializeApp(config);
+//   // Initialize Firebase
+//   var config = {
+//     apiKey: "AIzaSyAzLuXMlFXEt3-fUHbaF5BxNpb2_aJNw-Q",
+//     authDomain: "payinvoices-9d71a.firebaseapp.com",
+//     databaseURL: "https://payinvoices-9d71a.firebaseio.com",
+//     projectId: "payinvoices-9d71a",
+//     storageBucket: "payinvoices-9d71a.appspot.com",
+//     messagingSenderId: "767219632605"
+//   };
+//   firebase.initializeApp(config);
+
+  $(".payBtn" ).on("click", function() {
+      $(".modal").show();
+  });
+
   
+  $("#submitBtn").on("click", function(){
+    $(".modal").hide();
+  });
