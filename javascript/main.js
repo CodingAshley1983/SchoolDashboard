bc0f6189b63f118350993ba468c2640b213dfb3e
AshleyH <ladynirvana@hotmail.com>
2018-12-27 15:21:31 -0700

    built a goddamn calculator

--- javascript/main.js
+++ javascript/main.js
@@ -1,54 +1,85 @@
-function updateOutput(){
-    var a= $("input[name=10k]").val();
-    var b= $("input[name=25k]").val();
-    var c= $("input[name=insurance-cost]").val();
-    var d= $("input[name=premier-cost]").val();
-    var e= $("input[name=enhanced-cost]").val();
-    var f= $("input[name=standard-cost]").val();
-    console.log(a);
-    console.log(b);
-    console.log(c);
-    console.log (d);
-    console.log(e);
-    console.log(f);
+var a = $("input[name=10k]").val();
+var b = $("input[name=25k]").val();
+var c = $("input[name=insurance-cost]").val();
+var d = $("input[name=premier-cost]").val();
+var e = $("input[name=enhanced-cost]").val();
+var f = $("input[name=standard-cost]").val();
+var total = [];
 
-    if($("#bppcoverage-1").on("click", function(){
-        $("#bpp").append( "$ "+ a)
-    }));
-    
-    if($("#bppcoverage-2").on("click", function(){
-        $("#bpp").append("$ " + b)
-    }));
+//takes the form input data, appends it to the calculation modal, pushes the value to the 'total' array, calls the 
+//function that adds up the 'total' array
+function updateOutput() {
 
-    if($("#insurance-radio").on("click", function(){
-        console.log("you clicked insurance")
-        $("#insurance-p").append("$ " + c)
-    }));
-    if($("#premier").on("click", function(){
-        $("#dues-p").append("$ "+ d)
-    }));
-    if($("#enhanced").on("click", function(){
-        $("#dues-p").append("$ "+ e)
-    }));
-    if($("#standard").on("click", function(){
-        $("#dues-p").append("$ "+ f)
-    }));
+    if ($("#bppcoverage-1").on("click", function () {
+            $("#bpp").append("$ " + a);
+            total.push(a);
+            getSum();
 
+        }));
+
+    if ($("#bppcoverage-2").on("click", function () {
+            $("#bpp").append("$ " + b);
+            total.push(b);
+            getSum();
+        }));
+
+    if ($("#insurance-radio").on("click", function () {
+          
+            $("#insurance-p").append("$ " + c);
+            total.push(c);
+            getSum();
+        }));
+    if ($("#premier").on("click", function () {
+            $("#dues-p").append("$ " + d);
+            total.push(d);
+            getSum();
+        }));
+    if ($("#enhanced").on("click", function () {
+            $("#dues-p").append("$ " + e);
+            total.push(e);
+            getSum();
+        }));
+    if ($("#standard").on("click", function () {
+            $("#dues-p").append("$ " + f);
+            total.push(f);
+            getSum();
+        }));
+
+}
+
+
+//Looping over the 'total' array as new values are added, appending total to the page
+function getSum() {
+    var sum = 0
+    for (var i = 0; i < total.length; i++) {
+        sum += parseInt(total[i]);
     }
+    console.log(sum);
+    $("#total-p").text("$: " + sum + ".00")
 
-    $("#enroll-submit").on("click", function(){
-        $("#enrollment-modal").hide();
-        });
+}
 
-    $(document).ready(function() {
-     $("#enrollment-modal").show();
+$("#enroll-submit").on("click", function () {
+    $("#enrollment-modal").hide();
+
+});
+
+
+
+
+
+$(document).ready(function () {
+    $("#enrollment-modal").show();
     $("#calc-modal").show();
 
-    
+
+
     updateOutput();
 
 
 
-    });
-   
-   
+
+
+
+
+});
--- javascript/renewMembershipjs.js
+++ javascript/renewMembershipjs.js
@@ -1,10 +1,18 @@
 
+//need to set up AIE submission to nF, create assignment for School Relations Coordinator to confirm AIE 
+//Need to have Print Current COI button only light up if membership is active in nF, possibly create a date to select, pull COI report in PDF format, open in blank target, greyed out button
+//if not active- maybe make a note that if they've just renewed, their new COI will not print until the day after current policy ends
+
+//Receipt button only available for current, active membership. Will update with new policy receipt once it has been renewed.
+
 
 $("#addAIE").on("click", function(){
     console.log("button working")
     $("#aie-modal").show();
     
 });
+
+
 
 $("#aie-close").on("click", function(){
     $("#aie-modal").hide();
